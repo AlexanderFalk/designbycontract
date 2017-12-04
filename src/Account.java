32a3d8bf5c0f2612be12ac4ffdb7fe2f276a1b9e
Danny Nielsen <danny.nlsn0@gmail.com>
2017-12-04 15:41:17 +0100

    Deposit amount should always be positive

--- src/Account.java
+++ src/Account.java
@@ -10,10 +10,14 @@ public class Account {
         this.account_balance = 0;
     }
 
+    //@ requires amount > 0
     //@ assignable account_balance;
     //@ ensures account_balance \old(account_balance) + amount;
-    public double deposit(double amount) {
-        return this.account_balance += amount;
+    public double deposit(double amount) throws BankException {
+        if (amount > 0) return this.account_balance += amount;
+        else {
+            throw new BankException("The deposited amount must by larger than 0.");
+        }
     }
 
     //@ requires amount > 0 && account_balance > amount;
@@ -23,7 +27,7 @@ public class Account {
     //@ requires account_balance < amount
     //@ signals_only BankException(String message);
     public double withdraw(double amount) throws BankException {
-        if(amount <= this.account_balance)
+        if (amount <= this.account_balance)
             return this.account_balance -= amount;
         else {
             throw new BankException("The balance is too low compared to the withdrawn amount.");
@@ -31,7 +35,6 @@ public class Account {
     }
 
     //@ ensures \result == account_balance;
-    //@ pure
     public /*@ pure @*/ double getAccount_balance() {
         return account_balance;
     }
